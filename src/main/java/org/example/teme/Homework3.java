31fabc959cca3aa766e4ddc51f3f67739d473026
ADELINA-MARIA <carabatadelina23@gmail.com>
2023-10-14 23:57:38 +0300

    add rest of the document

--- src/main/java/org/example/teme/Homework3.java
+++ src/main/java/org/example/teme/Homework3.java
@@ -1,16 +1,51 @@
 package org.example.teme;
 
-public class Tema3 {
+public class Homework3 {
     //Fields
-
-    int field1;
-    double filed2;
-    long field3;
-    char field4;
-    boolean field5;
-    
-    String field8;
-
+    public int field1 = 0;
+    public double filed2;
+    public long field3;
+    public float field4;
+    public char field5;
+    public short field6;
+    public byte field7;
+    public boolean field8;
+    public String field9;
 
     //Methods
+    public void runHomework() {
+        System.out.println(field1);
+        field1++;
+        System.out.println(field1);
+        field1--;
+        System.out.println(field1);
+        ++field1;
+        System.out.println(field1);
+        --field1;
+        System.out.println(field1);
+        field1 = field1 + 2;
+        System.out.println(field1);
+        field1 += 2;
+        System.out.println(field1);
+        field1 -= 2;
+        System.out.println(field1);
+        field1 -= 5;
+        System.out.println(field1);
+        field1 *= 2;
+        System.out.println(field1);
+        field1 /= 2;
+        System.out.println(field1);
+        field1 %= 2;
+        System.out.println(field1);
+
+        if (field1 == -1) {
+            System.out.println("abc");
+        }
+        if (field1 != -2) {
+            System.out.println("bca");
+        }
+
+
+
+    }
 }
