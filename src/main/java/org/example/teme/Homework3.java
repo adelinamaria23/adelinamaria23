15da63fdc2cf4f77624ad42ca483cd158785a9af
ADELINA-MARIA <carabatadelina23@gmail.com>
2023-10-14 23:51:12 +0300

    First homework whit operators

---
+++ src/main/java/org/example/teme/Homework3.java
@@ -0,0 +1,16 @@
+package org.example.teme;
+
+public class Tema3 {
+    //Fields
+
+    int field1;
+    double filed2;
+    long field3;
+    char field4;
+    boolean field5;
+    
+    String field8;
+
+
+    //Methods
+}
